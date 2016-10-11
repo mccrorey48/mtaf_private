4cb8c43df8c8be066445b5bc8b124b8582297034
mmccrorey <mmccrorey@esi-estech.com>
2016-10-11 08:05:43 -0500

    add bas password/username steps

--- ccd/features/steps/login_steps.py
+++ ccd/features/steps/login_steps.py
@@ -12,9 +12,29 @@ def step_impl(context):
     login_view.input_reseller_username()
 
 
+@when('I enter a bad user ID')
+def step_impl(context):
+    login_view.input_bad_username()
+
+
+@when('I enter no user ID')
+def step_impl(context):
+    pass
+
+
 @when('I enter a valid password')
 def step_impl(context):
     login_view.input_reseller_password()
+
+
+@when('I enter a bad password')
+def step_impl(context):
+    login_view.input_bad_password()
+
+
+@when('I enter no password')
+def step_impl(context):
+    pass
 
 
 @when('I click the Login button')
@@ -25,3 +45,8 @@ def step_impl(context):
 @then('the Manager Home page will load')
 def step_impl(context):
     reseller_view.wait_for_page_title()
+
+
+@then('the invalid alert will appear')
+def step_impl(context):
+    login_view.wait_for_invalid_login_alert()
