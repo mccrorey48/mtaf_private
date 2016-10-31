b54abd5cc7a58d9be774647a85fa04e8ec60db8d
mmccrorey <mmccrorey@esi-estech.com>
2016-10-31 10:01:44 -0500

    add locators to domain_time_frames.py

--- ccd/views/domain_time_frames.py
+++ ccd/views/domain_time_frames.py
@@ -7,6 +7,14 @@ log = logging.get_logger('esi.domain_time_frames_view')
 
 class DomainTimeFramesView(DomainView):
 
+    locators = {
+        "TrashCanIconSub": {"by": "xpath", "value": "td[4]/a[2]"},
+        "AddTimeFrame": {"by": "xpath", "value": "//div[@id='content']/div/div[3]/a"},
+        "SaveButton": {"by": "id", "value": "SaveBtn"},
+        "TimeframeAddForm": {"by": "id", "value": "TimeframeAddForm"},
+        "TimeframeName": {"by": "id", "value": "TimeframeName"}
+    }
+
     def __init__(self):
         super(DomainTimeFramesView, self).__init__()
         self.view_name = "domain_time_frames"
