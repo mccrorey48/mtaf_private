53c0dd4e7bebbcadfe541a47536b95c5856659ab
Martin McCrorey <mmccrorey@esi-estech.com>
2016-11-21 13:34:08 -0600

    use per-server site settings for NullSound, DnsList, UseTcp

--- ePhone7/utils/get_softphone.py
+++ ePhone7/utils/get_softphone.py
@@ -2,8 +2,8 @@ from ePhone7.utils.configure import cfg
 from lib.softphone.softphone import Softphone
 
 
-def get_softphone(user_name=None, null_snd=False, tcp=False):
+def get_softphone(user_name=None):
     if user_name is None:
         user_name = cfg.site["DefaultSoftphoneUser"]
     user_cfg = cfg.site['Users'][user_name]
-    return Softphone(user_cfg, null_snd, tcp)
+    return Softphone(user_cfg)
