33adfdd33ab4732df8b3ffb5473d42b5b9b25ff4
Martin McCrorey <mmccrorey@esi-estech.com>
2017-05-30 08:52:34 -0500

    add advanced_settings_view

--- ePhone7/utils/ota.py
+++ ePhone7/utils/ota.py
@@ -7,18 +7,17 @@ from lib.user_exception import UserException as Ux
 
 @Trace(log)
 def set_alpha_ota_server():
-    dial_view.dial_name('Advanced Settings')
-    dial_view.touch_dial_button()
-    assert user_view.element_is_present('AdvancedOptions'), "Expected Advanced Options view to appear but it did not"
-    elems = user_view.find_named_elements('AdvancedItems')
+    dial_view.goto_advanced_settings()
+    assert advanced_settings_view.element_is_present('AdvancedOptions'), "Expected Advanced Options view to appear but it did not"
+    elems = advanced_settings_view.find_named_elements('AdvancedItems')
     assert len(elems) > 1
-    base_view.scroll(elems[-1], elems[0])
-    if not user_view.element_is_present('TestOtaServerUrlText'):
+    advanced_settings_view.scroll(elems[-1], elems[0])
+    if not advanced_settings_view.element_is_present('TestOtaServerUrlText'):
         # one retry in case the scroll didn't work
-        base_view.scroll(elems[-1], elems[0])
-    use_ota_text = user_view.find_named_element('UseTestOtaServerText')
+        advanced_settings_view.scroll(elems[-1], elems[0])
+    use_ota_text = advanced_settings_view.find_named_element('UseTestOtaServerText')
     text_ycenter = use_ota_text.location['y'] + (use_ota_text.size['height'] / 2)
-    checkboxes = base_view.find_named_elements('AdvancedCheckbox')
+    checkboxes = advanced_settings_view.find_named_elements('AdvancedCheckbox')
     for cb in checkboxes:
         min_y = cb.location['y']
         max_y = min_y + cb.size['height']
@@ -28,8 +27,8 @@ def set_alpha_ota_server():
         raise Ux('"Use Test OTA Server" checkbox not found')
     if cb.get_attribute('checked') == 'false':
         cb.click()
-    user_view.click_named_element('TestOtaServerUrlText')
-    ota_url = user_view.find_named_element('TestOtaEditText')
+    advanced_settings_view.click_named_element('TestOtaServerUrlText')
+    ota_url = advanced_settings_view.find_named_element('TestOtaEditText')
     ota_url.clear()
     ota_url.set_text('http://52.36.62.239/aus/')
 
