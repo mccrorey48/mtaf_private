edda99d49ca8483ec1f105f042f7985d62bbd120
Martin McCrorey <mmccrorey@esi-estech.com>
2017-07-10 11:28:06 -0500

    add feature & scenario to steps.txt, change context._config to context.config

--- ePhone7/features/environment.py
+++ ePhone7/features/environment.py
@@ -6,7 +6,7 @@ from ePhone7.views import base_view
 from ePhone7.utils.get_softphone import softphone_manager
 
 log = logging.get_logger('esi.environment')
-substeps=''
+substeps = ''
 
 
 def run_substep(context):
@@ -15,18 +15,21 @@ def run_substep(context):
         context.execute_steps(unicode('Then ' + step_name))
     return wrapped
 
+
 def before_all(context):
     global substeps
-    substeps=''
+    substeps = ''
     context.is_substep = False
     context.run_substep = run_substep(context)
-    tags = str(context._config.tags).split(',')
+    tags = str(context.config.tags).split(',')
     if 'fake' not in tags and 'json' not in tags:
         base_view.open_appium('nolaunch', force=True, timeout=60)
         base_view.startup()
 
 
 def before_feature(context, feature):
+    global substeps
+    substeps += "feature = %s\n" % feature.name
     logging.push_msg_src('feature')
     log.info('feature.name: %s' % feature.name)
 
@@ -36,6 +39,8 @@ def after_feature(context, feature):
 
 
 def before_scenario(context, scenario):
+    global substeps
+    substeps += "scenario = %s\n" % scenario.name
     logging.push_msg_src('  scenario')
     log.info('scenario.name: %s' % scenario.name)
 
@@ -43,7 +48,7 @@ def before_scenario(context, scenario):
 def after_scenario(context, scenario):
     softphone_manager.end_all_calls()
     softphone_manager.set_defaults()
-    tags = str(context._config.tags).split(',')
+    tags = str(context.config.tags).split(',')
     if 'fake' not in tags and 'json' not in tags:
         base_view.close_appium()
         base_view.open_appium()
@@ -70,7 +75,7 @@ def after_step(context, step):
         context.is_substep = False
     if step.exception:
         log.info("EXCEPTION in step %s" % step.name)
-    if 'fake' not in str(context._config.tags).split(','):
+    if 'fake' not in str(context.config.tags).split(','):
         if step.status == 'failed':
             xml = base_view.get_source()
             try:
@@ -88,7 +93,7 @@ def after_step(context, step):
 
 
 def after_all(context):
-    tags = str(context._config.tags).split(',')
+    tags = str(context.config.tags).split(',')
     if 'fake' not in tags and 'json' not in tags:
         base_view.close_appium()
     with open('tmp/steps.txt', 'w') as f:
