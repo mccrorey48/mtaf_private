308b5a73e080ddfd28c6d50bdf9274cc46abd3c2
Martin McCrorey <mmccrorey@esi-estech.com>
2017-08-25 14:06:19 -0500

    re_steps.py touch element by text now recognizes "tab" suffix and verifies active tab

--- ePhone7/features/steps/re_steps.py
+++ ePhone7/features/steps/re_steps.py
@@ -1,5 +1,5 @@
 from behave import *
-from ePhone7.views import base_view
+from ePhone7.views import base_view, user_view
 from lib.wrappers import fake
 
 use_step_matcher('re')
@@ -11,12 +11,15 @@ def a__with__ptext__appears(context, text):
     assert base_view.element_with_text_is_present(text)
 
 
-@step('I touch (?:[^"]*)?"(?P<text>[^"]+)"[^"]*')
+@step('I touch (?:[^"]*)?"(?P<text>[^"]+)"(?P<tail>[^"]*)')
 @fake
-def i_touch_ptext(context, text):
+def i_touch_ptext(context, text, tail):
     # matches: I touch <optional> "<text>" <optional>
     # where <optional> and <text> are any text not including a double quote
     # then looks for an element with full text content matching <text>
-    base_view.touch_element_with_text(text)
+    if tail == 'tab':
+        user_view.goto_tab(text)
+    else:
+        base_view.touch_element_with_text(text)
 
 
