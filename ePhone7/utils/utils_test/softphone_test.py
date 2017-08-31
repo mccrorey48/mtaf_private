f5697c84378f73d676ee31727a92de8ddbf489b0
Martin McCrorey <mmccrorey@esi-estech.com>
2017-08-31 16:11:42 -0500

    create call history for drs

--- ePhone7/utils/utils_test/softphone_test.py
+++ ePhone7/utils/utils_test/softphone_test.py
@@ -1,33 +1,39 @@
-from ePhone7.utils.get_softphone import get_softphone
+from ePhone7.utils.get_softphone import get_softphone, delete_softphone
 from time import sleep
 import lib.logging_esi as logging_esi
 from ePhone7.config.configure import cfg
 
 log = logging_esi.get_logger('esi.softphone_test')
 
+phones = {}
 logging_esi.console_handler.setLevel(logging_esi.TRACE)
-called = get_softphone('Auto TesterD')
-called.set_incoming_response(None)
-caller = get_softphone('Auto TesterC')
-log.debug("2202 making call")
-caller.make_call(called.uri)
-sleep(5)
-log.debug("2203 sending 100")
-called.send_response_code(100)
-sleep(5)
-log.debug("2203 sending 180")
-called.send_response_code(180)
-sleep(5)
-log.debug("2203 sending 200")
-called.send_response_code(200)
-sleep(5)
-log.debug("2203 executing hold")
-called.hold()
-sleep(5)
-log.debug("2203 executing unhold")
-called.unhold()
-sleep(5)
-log.debug("2202 ending call")
-caller.end_call()
-called.wait_for_call_status('idle', 5)
+users = sorted(cfg.site["DrsTestUsers"].keys())
+for index in range(len(users) / 2):
+    caller_name = users[index * 2]
+    called_name = users[index * 2 + 1]
+    print "%s --> %s" % (caller_name, called_name)
+    caller = get_softphone(caller_name, user_group="DrsTestUsers")
+    called = get_softphone(called_name, user_group="DrsTestUsers")
+    called.set_incoming_response(200)
+    caller.make_call(called.uri)
+    sleep(5)
+    caller.end_call()
+    called.wait_for_call_status('idle', 15)
+    delete_softphone(caller)
+    delete_softphone(called)
+# called.send_response_code(100)
+# sleep(5)
+# log.debug("2203 sending 180")
+# called.send_response_code(180)
+# sleep(5)
+# log.debug("2203 sending 200")
+# called.send_response_code(200)
+# sleep(5)
+# log.debug("2203 executing hold")
+# called.hold()
+# sleep(5)
+# log.debug("2203 executing unhold")
+# called.unhold()
+# sleep(5)
+# log.debug("2202 ending call")
 
