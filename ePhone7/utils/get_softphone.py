1e74d8493a8123c1b3b9b76f1ffa00b5742aa9e6
Martin McCrorey <mmccrorey@esi-estech.com>
2017-08-31 16:12:52 -0500

    parameterize user group, add delete_softphone function

--- ePhone7/utils/get_softphone.py
+++ ePhone7/utils/get_softphone.py
@@ -4,10 +4,10 @@ from lib.softphone.simple_pj import SoftphoneManager
 softphone_manager = SoftphoneManager()
 
 
-def get_softphone(user_name='default'):
+def get_softphone(user_name='default', user_group='Users'):
     if user_name == 'default':
         user_name = cfg.site['DefaultSoftphoneUser']
-    user_cfg = cfg.site['Users'][user_name]
+    user_cfg = cfg.site[user_group][user_name]
     uri = "sip:%s@%s" % (user_cfg['UserId'], user_cfg['DomainName'])
     proxy = user_cfg['Proxy']
     password = user_cfg['PhonePassword']
@@ -15,3 +15,7 @@ def get_softphone(user_name='default'):
     dns_list = cfg.site['DnsList']
     tcp = cfg.site['UseTcp']
     return softphone_manager.get_softphone(uri, proxy, password, null_snd, dns_list, tcp)
+
+
+def delete_softphone(softphone):
+    softphone_manager.delete_softphone(softphone)
