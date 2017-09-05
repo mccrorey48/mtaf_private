d8e1b60156f21aa48e495eb6336c5dc8833a5eca
Martin McCrorey <mmccrorey@esi-estech.com>
2017-09-05 14:05:45 -0500

    replace delete_softphone with unregister

--- ePhone7/utils/utils_test/softphone_test.py
+++ ePhone7/utils/utils_test/softphone_test.py
@@ -1,4 +1,4 @@
-from ePhone7.utils.get_softphone import get_softphone, delete_softphone
+from ePhone7.utils.get_softphone import get_softphone
 from time import sleep
 import lib.logging_esi as logging_esi
 from ePhone7.config.configure import cfg
@@ -8,19 +8,26 @@ log = logging_esi.get_logger('esi.softphone_test')
 phones = {}
 logging_esi.console_handler.setLevel(logging_esi.TRACE)
 users = sorted(cfg.site["DrsTestUsers"].keys())
-for index in range(len(users) / 2):
-    caller_name = users[index * 2]
-    called_name = users[index * 2 + 1]
+for index in range(0, len(users), 2):
+    caller_name = users[index]
+    called_name = users[(index + 2) % len(users)]
     print "%s --> %s" % (caller_name, called_name)
     caller = get_softphone(caller_name, user_group="DrsTestUsers")
     called = get_softphone(called_name, user_group="DrsTestUsers")
+    if caller.account_info.reg_status != 200:
+        print "%s reg status = %s, exiting" % (caller_name, caller.account_info.reg_status)
+        break
+    if called.account_info.reg_status != 200:
+        print "%s reg status = %s, exiting" % (called_name, called.account_info.reg_status)
+        break
     called.set_incoming_response(200)
     caller.make_call(called.uri)
     sleep(5)
     caller.end_call()
     called.wait_for_call_status('idle', 15)
-    delete_softphone(caller)
-    delete_softphone(called)
+    caller.unregister()
+    called.unregister()
+    sleep(2)
 # called.send_response_code(100)
 # sleep(5)
 # log.debug("2203 sending 180")
