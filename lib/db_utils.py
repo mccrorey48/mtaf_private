36f4145d37f17e1ca4cd846ac6b439b16c8cad1b
Martin McCrorey <mmccrorey@esi-estech.com>
2017-09-07 12:56:24 -0500

    add new drs users for svlab, remove drs_test_users collection

--- lib/db_utils.py
+++ lib/db_utils.py
@@ -92,10 +92,8 @@ def restore_dbs(_db_names, server, cfg_dir, _output_fd):
                     except KeyError:
                         pass
                     try:
-                        if doc['type'] == 'user':
+                        if doc['type'] == 'user' or doc['type'] == 'drs_test_user':
                             user.append(dict(doc, **{'site_tag': collection_name}))
-                        if doc['type'] == 'drs_test_user':
-                            drs_test_user.append(dict(doc, **{'site_tag': collection_name}))
                         elif doc['type'] == 'constants':
                             constants.append(dict(doc, **{'site_tag': collection_name}))
                     except KeyError:
