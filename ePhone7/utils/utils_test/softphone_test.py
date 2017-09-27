220ba2509390079873c3ad0c3d1792a186af141a
Martin McCrorey <mmccrorey@esi-estech.com>
2017-09-27 08:33:36 -0500

    modify softphone_test.py to trigger ePhone7 "no incoming call screen" bug

--- ePhone7/utils/utils_test/softphone_test.py
+++ ePhone7/utils/utils_test/softphone_test.py
@@ -17,17 +17,27 @@ sp_user_name = cfg.site['DefaultSoftphoneUser']
 sp_user_cfg = cfg.site['Users'][sp_user_name]
 sp_user_id = sp_user_cfg['UserId']
 uri = "sip:%s@%s" % (sp_user_id, sp_user_cfg['DomainName'])
-proxy = sp_user_cfg['Proxy']
+# proxy = sp_user_cfg['Proxy']
+proxy = 'nms-01.hs.cs.lax01.esihs.net'
 password = sp_user_cfg['PhonePassword']
 null_snd = cfg.site['NullSound']
 dns_list = cfg.site['DnsList']
 tcp = cfg.site['UseTcp']
 
 
-def call_e7_from_softphone():
+def call_e7_from_softphone(short_call=False):
     sp.make_call(e7_uri)
-    sleep(15)
-    sp.end_call()
+    sp.wait_for_call_status('early', timeout=30)
+    try:
+        if not short_call:
+            if not user_view.incoming_call_screen_is_present():
+                raise Ux("incoming call screen not present when ePhone7 is being called")
+            sleep(15)
+    except Ux:
+        raise
+    finally:
+        sp.end_call()
+        sp.wait_for_call_status('idle', timeout=10)
     sleep(5)
 
 
@@ -35,7 +45,7 @@ def call_softphone_from_e7():
     sleep(10)
     dial_view.dial_number(sp_user_id)
     dial_view.touch_dial_button()
-    sp.wait_for_call_status('call', timeout=30)
+    sp.wait_for_call_status('early', timeout=30)
     sleep(10)
     active_call_view.touch_end_call_button()
     sp.wait_for_call_status('idle', timeout=10)
@@ -49,18 +59,26 @@ try:
         raise Ux("%s reg status = %s, exiting" % sp.account_info.reg_status)
     base_view.open_appium()
     sleep(10)
+    call_count = 0
     # while True:
-    for i in range(20):
-        if not user_view.is_present():
-            log.info("Restarting Appium")
-            base_view.close_appium()
-            sleep(10)
-            base_view.open_appium()
-            sleep(10)
+    for i in range(18):
+        # if not user_view.is_present():
+        #     log.info("Restarting Appium")
+        #     base_view.close_appium()
+        #     sleep(10)
+        #     base_view.open_appium()
+        #     sleep(10)
         try:
-            user_view.goto_tab('Dial')
-            call_softphone_from_e7()
-            call_e7_from_softphone()
+            call_count += 1
+            # user_view.goto_tab('Dial')
+            # call_softphone_from_e7()
+            # call_e7_from_softphone()
+            if call_count % 6 == 0:
+                log.info("making normal call")
+                call_e7_from_softphone()
+            else:
+                log.info("making short call")
+                call_e7_from_softphone(short_call=True)
         except Ux as e:
             log.info("got user exception: %s" % e)
 except KeyboardInterrupt:
