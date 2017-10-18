aaf157372bdda403343d6772bf40783177863433
Martin McCrorey <mmccrorey@esi-estech.com>
2017-10-18 08:12:47 -0500

    refactor get_vmids

--- ePhone7/utils/get_vmids.py
+++ ePhone7/utils/get_vmids.py
@@ -11,10 +11,24 @@ log = logging.get_logger('esi.get_vmids')
 
 
 @Trace(log)
-def get_vmids(username, type):
-    type = type.lower()
-    if type not in ('new', 'saved', 'trash'):
-        raise Ux('get_vmids type must be new, saved or trash: got %s' % type)
+def get_first_vmid_data(username, category):
+    md = get_vm_metadata(category, username).json()[0]
+    return {'name': md['callerName'], 'number': md['callerNumber'], 'duration': md['duration'], 'timestamp': md['dateRecorded']}
+
+
+@Trace(log)
+def get_vmids(username, category):
+    rvvm = get_vm_metadata(category, username)
+    for vm in rvvm.json():
+        log.debug("get_vmids: %s vmid=%s" % (category, vm['vmid']))
+        print vm['dateRecorded']
+    return [vm['vmid'] for vm in rvvm.json()]
+
+
+def get_vm_metadata(category, username):
+    category = category.lower()
+    if category not in ('new', 'saved', 'trash'):
+        raise Ux('get_vmids category must be new, saved or trash: got %s' % category)
     user_cfg = cfg.site['Users'][username]
     roauth = requests.post(cfg.site["OauthURL"] + "/login", data=cfg.site["OauthUsername"] % (
         user_cfg['UserId'], user_cfg['DomainName'], user_cfg['AccountPassword']), headers=cfg.site["OauthHeaders"])
@@ -22,18 +36,16 @@ def get_vmids(username, type):
     vvm_headers = {key: cfg.site["VVMHeaders"][key] for key in cfg.site["VVMHeaders"]}
     vvm_headers["Authorization"] = vvm_headers["Authorization"] % access_token
     sleep(2)
-    rvvm = requests.get(cfg.site["VVMURL"] + "/new", headers=vvm_headers)
+    rvvm = requests.get(cfg.site["VVMURL"] + "/" + category, headers=vvm_headers)
     log.debug("rvvm = %s" % rvvm)
-    for vm in rvvm.json():
-        log.debug("get_vmids: %s vmid=%s" % (type, vm['vmid']))
-    return [vm['vmid'] for vm in rvvm.json()]
+    return rvvm
 
 
 @Trace(log)
-def vmid_count_incremented(username, type, old_vmid_count, timeout=60):
+def vmid_count_incremented(username, category, old_vmid_count, timeout=60):
     start_time = time()
     while time() - start_time < timeout:
-        new_vmid_count = len(get_vmids(username, type))
+        new_vmid_count = len(get_vmids(username, category))
         increase = new_vmid_count - old_vmid_count
         if increase == 0:
             sleep(10)
@@ -45,7 +57,9 @@ def vmid_count_incremented(username, type, old_vmid_count, timeout=60):
     else:
         return False
 
+
 if __name__ == "__main__":
-    vmids = get_vmids(cfg.site['DefaultForwardAccount'], 'new')
-    for vmid in vmids:
-        print vmid
+    print get_first_vmid_data('R2d2User', 'new')
+    # vmids = get_vmids('R2d2User', 'new')
+    # for vmid in vmids:
+    #     print vmid
