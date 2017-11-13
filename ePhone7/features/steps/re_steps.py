70a252eb038c568aa3ae0b7154a3b2c9c30c9bfb
Martin McCrorey <mmccrorey@esi-estech.com>
2017-11-13 08:58:36 -0600

    add "all_views" to ePhone7/views/__init__.py to simplify imports; new re_step implementations

--- ePhone7/features/steps/re_steps.py
+++ ePhone7/features/steps/re_steps.py
@@ -1,25 +1,56 @@
 from behave import *
-from ePhone7.views import base_view, user_view
+from ePhone7.views import all_views
 from lib.wrappers import fake
 
-use_step_matcher('re')
+use_step_matcher("re")
 
 
 @step('a (?:[^"]+) with (?:[^"]* )"(?P<text>[^"]+)"(?: [^"]*) appears')
 @fake
 def a__with__ptext__appears(context, text):
-    assert base_view.element_with_text_is_present(text)
+    assert all_views.base.element_with_text_is_present(text)
 
 
-@step('I touch (?:[^"]*)?"(?P<text>[^"]+)"(?P<tail>[^"]*)')
+@step("I send the (?P<name>\S+) keycode")
 @fake
-def i_touch_ptextptail(context, text, tail):
+def i_send_the_pnames_keycode(context, name):
+    all_views.base.send_keycode('KEYCODE_%s' % name.upper())
+
+
+@step('I touch (?:the )?"(?P<text>[^"]+)"(?P<tail>[^"]*)')
+@fake
+def i_touch_the_ptextptail(context, text, tail):
     # matches: I touch <optional> "<text>" <optional>
     # where <optional> and <text> are any text not including a double quote
     # then looks for an element with full text content matching <text>
     if tail == 'tab':
-        user_view.goto_tab(text)
+        all_views.user.goto_tab(text)
     else:
-        base_view.touch_element_with_text(text)
+        all_views.base.touch_element_with_text(text)
 
 
+@step('the "(?P<view>\S+)" view (?P<mode>dis)?appears')
+@fake
+def step_impl(context, view, mode):
+    if mode == 'dis':
+        context.run_substep('the "%s" view is not present' % view)
+    else:
+        context.run_substep('the "%s" view is present' % view)
+
+
+@step('the "(?P<view>\S+)" view is (?P<mode>\S*)\s?present')
+@fake
+def the_pviews_view_is_pmodesspresent(context, view, mode):
+    if mode.lower() == 'not':
+        assert all_views[view].becomes_not_present(), "%s view is present"
+    else:
+        assert all_views[view].becomes_present(), "%s view is not present"
+
+
+@step("the (?P<view>\S+) view (?P<name>\S+) element is (?P<mode>\S*)\s?present")
+@fake
+def the_pviews_view_pnames_element_is_pmodesspresent(context, view, name, mode):
+    if mode.lower() == 'not':
+        assert all_views[view][name] is None, '%s view element "%s" is present' % (view, name)
+    else:
+        assert all_views[view][name], '%s view element "%s" is not present' % (view, name)
--- ePhone7/views/__init__.py
+++ ePhone7/views/__init__.py
@@ -1,13 +1,54 @@
-from .base_view import base_view
-from .login_view import login_view
-from .tnc_view import tnc_view
-from .app_intro_view import app_intro_view
-from .user_view import user_view
-from .contacts_view import contacts_view
-from .history_view import history_view
-from .voicemail_view import voicemail_view
-from .dial_view import dial_view
-from .prefs_view import prefs_view
-from .network_view import network_view
-from .advanced_settings_view import advanced_settings_view
 from .active_call_view import active_call_view
+from .advanced_settings_view import advanced_settings_view
+from .app_intro_view import app_intro_view
+from .base_view import base_view
+from .contacts_view import contacts_view
+from .dial_view import dial_view
+from .history_view import history_view
+from .home_view import home_view
+from .login_view import login_view
+from .network_view import network_view
+from .prefs_view import prefs_view
+from .tnc_view import tnc_view
+from .user_view import user_view
+from .voicemail_view import voicemail_view
+
+view_dict = {
+    'active_call': active_call_view,
+    'active call': active_call_view,
+    'advanced_settings': advanced_settings_view,
+    'advanced settings': advanced_settings_view,
+    'app_intro': app_intro_view,
+    'app intro': app_intro_view,
+    'base': base_view,
+    'contacts': contacts_view,
+    'dial': dial_view,
+    'history': history_view,
+    'home': home_view,
+    'login': login_view,
+    'network': network_view,
+    'prefs': prefs_view,
+    'preferences': prefs_view,
+    'tnc': tnc_view,
+    'user': user_view,
+    'voicemail': voicemail_view
+}
+
+
+class AllViews(object):
+    def __getitem__(self, view_name):
+        return self.__getattr__(view_name)
+
+    def __getattr__(self, view_name):
+        view_name = view_name.lower()
+        if view_name[-4:] == 'view':
+            view_name = view_name[:-4]
+        elif view_name[-5:] == '_view':
+            view_name = view_name[:-5]
+        if view_name in view_dict:
+            return view_dict[view_name]
+        else:
+            return None
+
+
+all_views = AllViews()
