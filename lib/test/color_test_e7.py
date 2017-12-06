c4b71043e9d31366bd5601fe4c7f234be63ad735
Martin McCrorey <mmccrorey@esi-estech.com>
2017-12-06 14:05:01 -0600

    fix color_test_e7.py

--- lib/test/color_test_e7.py
+++ lib/test/color_test_e7.py
@@ -1,3 +1,11 @@
-from ePhone7.views.voicemail_view import voicemail_view
-
-voicemail_view.actions.get_tab_color('voicemail', 'Saved')
+from ePhone7.config.configure import cfg
+from ePhone7.views import *
+base_view.open_appium()
+base_view.touch_element_with_text('Voicemail')
+if voicemail_view.becomes_present():
+    voicemail_view.get_screenshot_as_png('voicemail', cfg.test_screenshot_folder)
+    print voicemail_view.get_tab_color('voicemail', 'New')
+    print voicemail_view.get_tab_color('voicemail', 'Saved')
+    print voicemail_view.get_tab_color('voicemail', 'Trash')
+else:
+    print "voicemail view did not appear"
