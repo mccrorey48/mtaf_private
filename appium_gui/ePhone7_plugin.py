6ff4965083f65374727d22f86eae5644887b0d96
Martin McCrorey <mmccrorey@esi-estech.com>
2017-12-12 09:22:03 -0600

    pass parent instance to E7Commands constructor

--- appium_gui/ePhone7_plugin.py
+++ appium_gui/ePhone7_plugin.py
@@ -2,11 +2,15 @@ from ePhone7.views import *
 from ePhone7.utils.usb_enable import usb_enable
 from ePhone7.utils.versions import get_installed_versions
 from ePhone7.config.configure import cfg
+from ePhone7.utils.spud_serial import SpudSerial
 from ePhone7.utils.get_softphone import get_softphone
 from Tkinter import Frame, IntVar, StringVar, Button, Checkbutton, Label, DISABLED, NORMAL
 
 
 class E7Commands(object):
+
+    def __init__(self, parent):
+        self.parent = parent
 
     @staticmethod
     def dial_beta_ota():
@@ -17,7 +21,6 @@ class E7Commands(object):
         dial_view.touch_call_button()
         print "Done"
 
-
     @staticmethod
     def dial_alpha_ota():
         print "dialing alpha OTA code...",
@@ -27,7 +30,6 @@ class E7Commands(object):
         dial_view.touch_call_button()
         print "Done"
 
-
     @staticmethod
     def dial_prod_ota():
         print "dialing prod OTA code...",
@@ -35,7 +37,6 @@ class E7Commands(object):
         dial_view.dial_set_production_ota_server()
         dial_view.touch_call_button()
         print "Done"
-
 
     @staticmethod
     def dial_show_ota():
@@ -45,7 +46,6 @@ class E7Commands(object):
         dial_view.touch_call_button()
         print "Done"
 
-
     @staticmethod
     def dial_advanced_settings():
         print "dialing advanced settings code...",
@@ -54,7 +54,6 @@ class E7Commands(object):
         dial_view.touch_call_button()
         print "Done"
 
-
     @staticmethod
     def get_installed_versions():
         print "getting installed versions"
@@ -62,18 +61,24 @@ class E7Commands(object):
         aosp, app = get_installed_versions()
         print "aosp: %s, app: %s" % (aosp, app)
 
-
     @staticmethod
     def set_favorites():
         print "setting all favorite coworkers...",
         contacts_view.set_all_favorites()
         print "Done"
 
-
     @staticmethod
     def usb_enable():
         print "Enabling USB via spud port...",
         usb_enable()
+        print "Done"
+
+    def reboot(self):
+        print "rebooting...",
+        ss = SpudSerial(cfg.site['SerialDev'])
+        self.parent.log_action(ss, {'cmd': 'cd\n', 'new_cwd': 'data'})
+        self.parent.log_action(ss, {'cmd': 'reboot\n', 'new_cwd': '', 'expect': 'mtp_open', 'dead_air_timeout': 20,
+                                    'timeout': 120})
         print "Done"
 
 #     def create_softphone_frame(self):
@@ -159,43 +164,41 @@ class E7Commands(object):
 #         self.after(100, self.check_status)
 
 
-e7_cmds = E7Commands()
-
-cmd_types = {
-    'Appium Actions': [
-        {'label': 'Dial Advanced Settings Code', 'command': e7_cmds.dial_advanced_settings},
-        {'label': 'Dial Alpha OTA Code', 'command': e7_cmds.dial_alpha_ota},
-        {'label': 'Dial Beta OTA Code', 'command': e7_cmds.dial_beta_ota},
-        {'label': 'Dial Current OTA Code', 'command': e7_cmds.dial_show_ota},
-        {'label': 'Dial Production OTA Code', 'command': e7_cmds.dial_prod_ota},
-        # {'label': 'Create Softphones', 'command': e7_cmds.create_softphone_frame},
-        {'label': 'Set Coworker Favorites', 'command': e7_cmds.set_favorites}
-    ],
-    'Other Actions': [
-        {'label': 'Enable USB', 'command': e7_cmds.usb_enable}
-    ]
-}
-
-
 # appium_gui.py instantiates the AppiumGui class, then calls install(), passing the AppiumGui instance as an argument
-def install(self):
+def install(parent):
 
     # - install drop-down menu items appropriate for ePhone7
     # - "cmd_types" key values are the labels for drop-down menus in AppiumGui
     # - new commands listed in "cmd_types[key]" are added to the menu labeled with that key
     # - if there is no drop-down menu labeled with a key, a new drop-down menu will be created
+    e7_cmds = E7Commands(parent)
+    cmd_types = {
+        'Appium Actions': [
+            {'label': 'Dial Advanced Settings Code', 'command': e7_cmds.dial_advanced_settings},
+            {'label': 'Dial Alpha OTA Code', 'command': e7_cmds.dial_alpha_ota},
+            {'label': 'Dial Beta OTA Code', 'command': e7_cmds.dial_beta_ota},
+            {'label': 'Dial Current OTA Code', 'command': e7_cmds.dial_show_ota},
+            {'label': 'Dial Production OTA Code', 'command': e7_cmds.dial_prod_ota},
+            # {'label': 'Create Softphones', 'command': e7_cmds.create_softphone_frame},
+            {'label': 'Set Coworker Favorites', 'command': e7_cmds.set_favorites}
+        ],
+        'Other Actions': [
+            {'label': 'Enable USB', 'command': e7_cmds.usb_enable},
+            {'label': 'Reboot', 'command': e7_cmds.reboot}
+        ]
+    }
     for cmd_type in cmd_types:
-        if cmd_type not in self.menu.submenus:
-            self.menu.add_submenu(cmd_type)
+        if cmd_type not in parent.menu.submenus:
+            parent.menu.add_submenu(cmd_type)
         for cmd_spec in cmd_types[cmd_type]:
-            self.menu.submenus[cmd_type].add_command(label=cmd_spec['label'], command=cmd_spec['command'])
+            parent.menu.submenus[cmd_type].add_command(label=cmd_spec['label'], command=cmd_spec['command'])
 
     # add locator type options for variouis ePhone7 views
-    values = self.btn_frame.find_frame.by.cget('values')
+    values = parent.btn_frame.find_frame.by.cget('values')
     values += ('contacts_locator', 'voicemail_locator', 'history_locator',
                'dial_locator', 'prefs_locator', 'contacts_locator_all',
                'voicemail_locator_all', 'history_locator_all', 'dial_locator_all',
                'prefs_locator_all')
-    self.btn_frame.find_frame.by.configure(values=values)
+    parent.btn_frame.find_frame.by.configure(values=values)
 
 
