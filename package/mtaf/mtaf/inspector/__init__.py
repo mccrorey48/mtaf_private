4dde996189984bad4b7ec238a5b4d7bacc1f7065
Martin McCrorey <mmccrorey@esi-estech.com>
2018-01-08 08:23:20 -0600

    change "start()" arg from cfg dict to "key=value" pairs, remove default cfg dict from __init__.py

--- package/mtaf/mtaf/inspector/__init__.py
+++ package/mtaf/mtaf/inspector/__init__.py
@@ -1,13 +1,14 @@
-from inspector import run_inspector
-
-default_cfg = {
-    'csv_dir': '.',
-    'xml_dir': '.',
-    'screenshot_dir': '.',
-    'log_dir': '.',
-    'tmp_dir': '.'
-}
+from mtaf.inspector.inspector import run_inspector
+import sys
+from mtaf.lib.user_exception import UserException as Ux
 
 
-def start(cfg=default_cfg):
+def start():
+    args = sys.argv[1:]
+    cfg = {}
+    for arg in args:
+        terms = arg.split('=')
+        if len(terms) != 2:
+            raise Ux("arguments to start() must be of the form <key>=<value>, with no spaces")
+        cfg[terms[0]] = terms[1]
     run_inspector(cfg)
