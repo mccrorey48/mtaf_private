036054f02e406cd1a6b9d58f7f4b947c7988806d
Martin McCrorey <mmccrorey@esi-estech.com>
2018-01-11 13:01:11 -0600

    delete run_inspector/inspector_plugins, bump version to 11

--- package/mtaf/setup.py
+++ package/mtaf/setup.py
@@ -13,7 +13,7 @@ setup(
     name='mtaf',
     packages=find_packages(),
     author='Martin McCrorey',
-    version='1.0.10',
+    version='1.0.11',
     license='MIT',
     url='https://github.com/mccrorey48/mtaf',
     description='Mobile Test Automation Framework',
