a6c1b0ea60084b9272cca2a54cd9d259648112da
Martin McCrorey <mmccrorey@esi-estech.com>
2018-03-21 07:49:39 -0500

    test lab microservice instead of public

--- lib/test/microservice_test.py
+++ lib/test/microservice_test.py
@@ -3,12 +3,12 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument("login", type=str, help="<phone number>@<domain>")
-parser.add_argument('password', help='password',
-                    default='vqda')
+parser.add_argument('password', help='password')
 args = parser.parse_args()
 
 print "login = %s" % args.login
 print "password = %s" % args.password
-ms = Microservices(args.login, args.password, 'https://pro.esiapi.io/')
+# URL is 10.3.1.5 for svlab, pro.esiapi.io for production
+ms = Microservices(args.login, args.password, 'http://10.3.1.5/')
 for cat in 'new', 'saved', 'deleted':
     print "%s %s" % (cat, ms.get_vm_count(cat))
