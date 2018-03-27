4e9c00f66dcb575e03bd4114b02961d452bd524c
Martin McCrorey <mmccrorey@esi-estech.com>
2018-03-27 13:44:43 -0500

    bump mtaf version to 1.0.38

--- package/mtaf/setup.py
+++ package/mtaf/setup.py
@@ -14,7 +14,7 @@ setup(
     name='mtaf',
     packages=find_packages(),
     author='Martin McCrorey',
-    version='1.0.38',
+    version='1.0.39',
     license='MIT',
     url='https://github.com/mccrorey48/mtaf',
     description='Mobile Test Automation Framework with Appium Inspector GUI for Android Applications',
