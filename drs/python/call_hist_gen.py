665a6c9bbd78ada15ca882aa6d1eb79fb57e50c2
Martin McCrorey <mmccrorey@esi-estech.com>
2018-05-15 14:06:41 -0500

    set up call_hist_gen.py to read account passwords from nms device page export and register phones (no calls)

--- drs/python/call_hist_gen.py
+++ drs/python/call_hist_gen.py
@@ -1,62 +1,72 @@
-from ePhone7.lib.utils.get_softphone import get_softphone
-from time import sleep, time
-from mtaf import mtaf_logging
-from ePhone7.config.configure import cfg
+from mtaf.simple_pj import SoftphoneManager
+from time import sleep
+import os
+from six import print_
+import csv
 
-log = mtaf_logging.get_logger('mtaf.softphone_test')
+# reads a csv file for multiple user account uri and passwords
+# (the csv file is exported by the nms from the "devices" page)
+# then registers up to max_user_count phones concurrently;
+# after a sleep, unregisters the phones and exits
+#
+# this script uses mtaf libraries to create softphones; it must be run
+# from the top level "mtaf" directory in the new_arch branch of the
+# mtaf repo (clone from https://bitbucket.org/estech/mtaf)
+#
+# the recommended way to set up the mtaf as a runtime environment
+# is:
+#
+# - start with a clean Python virtualenv
+# - clone the mtaf repo
+# - check out the new_arch branch
+# - cd to mtaf/packages/mtaf
+# - enter ". develop" to make the mtaf package accessible for imports
+# - build the pjsip python API (see MTAF confluence pages for help) and
+#   install into the virtualenv so softphones can be instantiated
 
+softphone_manager = SoftphoneManager()
+
+
+def get_softphone(uri, password):
+    proxy = 'nr5.cpbx.esihs.net'
+    null_snd = True
+    dns_list = ['10.0.50.156']
+    tcp = False
+    reg_wait = False
+    return softphone_manager.get_softphone(uri, proxy, password, null_snd, dns_list, tcp, reg_wait)
+
+
+passwords={}
+with open(os.path.join('drs', 'python', 'users.csv')) as f:
+    reader = csv.DictReader(f, quotechar="'")
+    for row in reader:
+        uri = row['aor']
+        if uri.split('@')[0][-1] != 'a':
+            passwords[uri] = row['authentication_key']
+uris = sorted(passwords.keys())
 max_user_count = 50
-
-production_proxy_override = 'nms-21.hs.cs.jfk01.esihs.net'
-# production_proxy_override = None
-
-if cfg.site_tag == 'mm':
-    proxy_override = production_proxy_override
-else:
-    proxy_override = None
-phones = {}
-mtaf_logging.console_handler.setLevel(mtaf_logging.TRACE)
-users = [user for user in sorted(cfg.site["Users"].keys()) if 'type' in cfg.site["Users"][user]
-         and cfg.site["Users"][user]['type'] == 'drs_test_user']
 softphone_pairs = []
-# for index in range(0, len(users), 2):
 for index in range(0, max_user_count, 2):
-    if index > len(users):
+    if index > len(uris):
         break
-    caller_name = users[index]
-    called_name = users[(index + 1) % len(users)]
-    log.warn("%s --> %s" % (caller_name, called_name))
-    caller = get_softphone(caller_name, user_group="Users", reg_wait=False, proxy_override=proxy_override)
-    called = get_softphone(called_name, user_group="Users", reg_wait=False, proxy_override=proxy_override)
+    caller_uri = uris[index]
+    called_uri = uris[(index + 1) % len(uris)]
+    print_("%s --> %s" % (caller_uri, called_uri))
+    caller = get_softphone(caller_uri, passwords[caller_uri])
+    called = get_softphone(called_uri, passwords[called_uri])
     softphone_pairs.append({"caller": caller, "called": called})
 sleep(2)
-# for pair in softphone_pairs:
-#     pair["called"].set_incoming_response(200)
-#     pair["caller"].make_call(pair["called"].uri)
-sleep(300)
-# for pair in softphone_pairs:
-#     log.warn("caller %s call time: %s" % (pair["caller"].account_info.uri,
-#                                           time() - pair["caller"].account_info.call_start_time))
-#     log.warn("called %s call time: %s" % (pair["called"].account_info.uri,
-#                                           time() - pair["called"].account_info.call_start_time))
-#     pair["caller"].end_call()
-#     pair["called"].wait_for_call_status('idle', 15)
+# # for pair in softphone_pairs:
+# #     pair["called"].set_incoming_response(200)
+# #     pair["caller"].make_call(pair["called"].uri)
+sleep(10)
+# # for pair in softphone_pairs:
+# #     print_("caller %s call time: %s" % (pair["caller"].account_info.uri,
+# #                                           time() - pair["caller"].account_info.call_start_time))
+# #     print_("called %s call time: %s" % (pair["called"].account_info.uri,
+# #                                           time() - pair["called"].account_info.call_start_time))
+# #     pair["caller"].end_call()
+# #     pair["called"].wait_for_call_status('idle', 15)
 for pair in softphone_pairs:
     pair["caller"].unregister()
     pair["called"].unregister()
-sleep(2)
-# called.send_response_code(100)
-# sleep(5)
-# log.debug("2203 sending 180")
-# called.send_response_code(180)
-# sleep(5)
-# log.debug("2203 sending 200")
-# called.send_response_code(200)
-# sleep(5)
-# log.debug("2203 executing hold")
-# called.hold()
-# sleep(5)
-# log.debug("2203 executing unhold")
-# called.unhold()
-# sleep(5)
-# log.debug("2202 ending call")
